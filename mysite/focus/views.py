642b01b69f0e853073a837c27f0f8bfa0ceb6dd4
LLLPPP <928239280@qq.com>
2016-12-13 12:06:22 +0800

    improve register and login

--- mysite/focus/views.py
+++ mysite/focus/views.py
@@ -1,12 +1,12 @@
 #coding=utf-8
 from django.http import HttpResponse,HttpResponseRedirect
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,render_to_response
 from django.contrib.auth import login,logout,authenticate
 from django.conf import settings
 from focus.models import *
 from focus.forms import *
 from django.contrib.auth.hashers import make_password ###密码加密的处理方法
-from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required
 import pdb
 
 
@@ -40,33 +40,39 @@ def services(request):
 def do_reg(request):
 	try:
 		if request.method == "POST":
+			print "aaa"
 			reg_form = RegForm(request.POST)
 			if reg_form.is_valid():
 				"""注册"""
+				print "bbb"
 				user = NewUser.objects.create(username=reg_form.cleaned_data["username"],
 				                           email = reg_form.cleaned_data["email"],
-				                           url = reg_form.cleaned_data["url"],
+				                           #profile = reg_form.cleaned_data["profile"],
 				                           password =make_password(reg_form.cleaned_data["password"]),)
 				user.save()
 
 				#登录
 				user.backend="django.contrib.auth.backends.ModelBackend"###指定默认的登录验证方式
+				print "ccc"
 				login(request,user)
-				return HttpResponseRedirect(request.POST.get("source_url"))
+				print request.user
+				return HttpResponseRedirect(request.POST.get("source_url"))  # 登录成功后重定向到之前的页面
 			else:
 				return render(request,"failure.html",{"reason":reg_form.errors})
 		else:
+			#request.session['reg_from'] = request.META.get('HTTP_REFERER', '/')
 			reg_form = RegForm()
+			print "eee"
 	except Exception as e:
 		print e
-	return  render(request,"reg.html",locals())
+	return  render(request,"reg.html",{"reg_form":reg_form})
 
 def add_user(request):
 	if request.method == "POST":
 		comment_form = CommentForm(request.POST)
 		if comment_form.is_valid():
 			comment_form.save()
-			return HttpResponseRedirect ("index")
+			return HttpResponseRedirect (request.POST.get("source_url"))
 
 	else:
 		user_form = CommentForm()
@@ -83,19 +89,21 @@ def do_login(request):
 				username = login_form.cleaned_data["username"]
 				password = login_form.cleaned_data["password"]
 				user = authenticate(username=username,password=password)###django提供的验证方法
-				if user is not None:
-					user.backend = "django.contrib.auth.backend.ModelBackend"
+				if user is not None and user.is_active:
+					#user.backend = "django.contrib.auth.backend.ModelBackend"
 					print "ddd"
 					login(request,user)
-					return HttpResponseRedirect(request.session['login_from'])#登录成功后重定向到之前的页面
+
+					print bool(request.user.is_authenticated)
 				else:
 					print "eee"
 					return render(request, 'login.html', {"login_form":login_form, 'error': "password or username is not ture!"})
+				return redirect(request.session['login_from'])  # 登录成功后重定向到之前的页面
 			else:
 				return render(request,"failure.html",{"reason":login_form.errors})
 		else:
-			login_form = LoginForm()
 			request.session['login_from'] = request.META.get('HTTP_REFERER', '/')#记住get方法获得的页面，待会儿回到这个页面，没有就设置为主页
+			login_form = LoginForm()
 	except Exception as e:
 		print e
 	return render(request,"login.html",{"login_form":login_form})
@@ -106,4 +114,4 @@ def do_logout(request):
 		logout(request)
 	except Exception as e:
 		print e
-	return render(request.META["HTTP_REFERER"])
+	return render(request.POST.get("source_url"),"/")
